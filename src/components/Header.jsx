513f2b974e1863dea6709e68ff7377813569ff78
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-08 10:18:24 -0700

    Create Header component.

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.header`
+  width: 100%;
+  height: 40px;
+  background: blue;
+`;
+
+function Header() {
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  )
+}
+
+export default Header;
