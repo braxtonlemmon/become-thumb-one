264e99f12609a5bde3c256484b0bcd5c2b347588
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-08 10:55:08 -0700

    Create About (a.k.a. 'Huh?') and Contact (a.k.a. 'Yo') pages.

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+function AboutPage() {
+  return (
+    <Layout>
+      <SEO title="About" />
+      <p>about</p>
+    </Layout>
+  )
+}
+
+export default AboutPage;
