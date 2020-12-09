d8284a05ad2fee2315a9387a385eebd9dba9c047
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-08 17:17:19 -0700

    Subscribe to context store in Dashboard.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
+import { Link, navigate } from 'gatsby';
+import { Context } from '../context/GlobalContext';
 
 const Wrapper = styled.div`
-
+  display: flex;
+  flex-direction: column;
 `;
 
 function Dashboard() {
+  const { 
+    name, 
+    setName,
+    requiredReadingDone,
+    hearingTestDone,
+    clickAButtonDone,
+    thumbSingingDone
+  } = useContext(Context);
+  
   return (
     <Wrapper>
-      Dashboard
+      Dashboard {name}
+      <p onClick={() => {
+        setName('jo')
+      }}>Change</p>
       <Link to="/required-reading">Required reading</Link>
       <Link to="/hearing-test">Hearing test</Link>
       <Link to="/click-a-button">Click a button</Link>
       <Link to="/thumb-singing">Thumb singing</Link>
+      <p>{requiredReadingDone ? 'reading done' : 'reading not done'}</p>
     </Wrapper>
   )
 }
