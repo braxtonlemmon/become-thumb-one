ecc180125eb1963909b1b93184d53ff39394f566
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-09 12:45:09 -0700

    Create directory to house needed data for the site.

---
+++ src/data/BookData.js
@@ -0,0 +1,24 @@
+const BookData = [
+  {
+    id: 0,
+    title: 'Snow Beetle',
+    text: 'This is a book about the snow beetle.'
+  },
+  {
+    id: 1,
+    title: 'The Consciousness Stream',
+    text: 'This is a book written in stream of consciousness style.'
+  },
+  {
+    id: 2,
+    title: 'Running Through Sliding Doors',
+    text: 'This is the story of when I ran through sliding doors.'
+  },
+  {
+    id: 3,
+    title: 'Holy Socks',
+    text: 'This is all about my socks that have holes in them.'
+  }
+]
+
+export default BookData;
