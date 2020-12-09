6f3f88458bcf30880475ea501df70c97a8fd3b77
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-09 09:50:40 -0700

    Installed react-hook-form. Setup the structure for the GetToKnowYou form.

---
+++ src/components/GetToKnowYouForm.jsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+
+const Wrapper = styled.div`
+
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin: 15px 5px;
+`;
+
+function GetToKnowYouForm() {
+  const { register, handleSubmit, errors } = useForm({
+    defaultValues: {
+      name: '',
+      age: 5,
+    }
+  })
+
+  const onSubmit = (data, e) => {
+    console.log('done');
+  }
+
+  const getMaxAge = () => {
+    let date = new Date();
+    return date.getFullYear() - 1973;
+  }
+
+  return (
+    <Wrapper>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Block>
+          <label htmlFor="sofa">
+            Of the following, which is your favorite sofa type?
+          </label>
+          <select
+            name="sofa"
+            ref={register({
+              required: "Select one option"
+            })}>
+              <option value=""></option>
+              <option value="futon">Denim Futon</option>
+              <option value="banana">White Leather Banana Chair</option>
+              <option value="bench">Cast Iron Park Bench</option>
+              <option value="sectional">Beige Sectional</option>
+          </select>
+        </Block>
+
+
+        <Block>
+          <label htmlFor="age">How old are you? (Age entered must be less than someone born in 1973.)</label>
+          <input
+            type="number"
+            name="age"
+            id="age"
+            ref={
+              register({
+                required: 'Age is required',
+                max: getMaxAge(),
+              })
+            }
+          />
+        </Block>
+
+
+        <Block>
+          <h3>Are you lying about your age?</h3>
+          <label>
+            <input 
+              type="radio"
+              name="trueAge"
+              value="Yes"
+              checked={true}
+              ref={register}
+            />
+            Yes
+          </label>
+          <label>
+            <input 
+              type="radio"
+              name="trueAge"
+              value="No"
+              ref={register}
+            />
+            No
+          </label>
+        </Block>
+        
+        <Block>
+          <label htmlFor="name">What is your name? (Name entered must be between 5 and 7 characters.)</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            ref={
+              register({ 
+                required: 'First name is required',
+                minLength: 5,
+                maxLength: 7
+              })
+            }
+          />
+        </Block>
+
+        <Block>
+          <h3>Do you enjoy the occasional deep-fried Twinkie or pickle?</h3>
+          <label>
+            <input
+              type="radio"
+              name="enjoyFried"
+              value="Yes"
+              checked={true}
+              ref={register}
+            />
+            Yes
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="enjoyFried"
+              value="No"
+              ref={register}
+            />
+            No
+          </label>
+        </Block>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Wrapper>
+  )
+}
+
+export default GetToKnowYouForm;
--- src/pages/get-to-know-you.js
+++ src/pages/get-to-know-you.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import FormContainer from '../components/GetToKnowYou/FormContainer';
+import GetToKnowYouForm from '../components/GetToKnowYouForm';
 
 function GetToKnowYouPage() {
   return (
     <Layout>
       <SEO title="Get to know you" />
-      <p>GetToKnowYouPage</p>
-      <form>
-      
-      </form>
+      {/* <FormContainer /> */}
+      <GetToKnowYouForm />
     </Layout>
   )
 }
