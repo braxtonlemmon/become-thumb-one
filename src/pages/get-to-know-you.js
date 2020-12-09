3c55ec02b3d9ceb8b54b974a10a8cd71e88f615d
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-08 17:16:21 -0700

    Add filler content to get-to-know-you and required-reading

---
+++ src/pages/get-to-know-you.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+function GetToKnowYouPage() {
+  return (
+    <Layout>
+      <SEO title="Get to know you" />
+      <p>GetToKnowYouPage</p>
+      <form>
+      
+      </form>
+    </Layout>
+  )
+}
+
+export default GetToKnowYouPage;
--- src/pages/required-reading.js
+++ src/pages/required-reading.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { Context } from '../context/GlobalContext';
 
 function RequiredReadingPage() {
+  const { setRequiredReadingDone } = useContext(Context);
+  
   return (
     <Layout>
       <SEO title="Thumb Singing" />
       <p>RequiredReadingPage</p>
+      <p onClick={() => {
+        setRequiredReadingDone(true);
+      }}>click</p>
     </Layout>
   )
 }
