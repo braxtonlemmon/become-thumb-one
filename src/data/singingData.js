8faa9a92f04280313c94cbacf2a1f35c0b0a8827
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-11 16:43:04 -0700

    Change singingData songs to songs that are withing public domain. Gotta be legal.

--- src/data/singingData.js
+++ src/data/singingData.js
@@ -1,114 +1,224 @@
 const singingData = [
   {
     id: 0,
-    title: 'Thumbwhere Over the Rainbow',
-    intro: 'Thumbwhere over the rainbow, way up...',
+    title: 'Take Me Out to the Ball Game',
+    intro: 'Take me out to the ball game...',
     words: [
       {
         id: 1,
-        word: 'high'
+        word: 'take'
       },
       {
         id: 2,
-        word: 'and'
+        word: 'me (1st)'
       },
       {
         id: 3,
-        word: 'the'
+        word: 'out'
       },
       {
         id: 4,
-        word: 'dreams'
+        word: 'with'
       },
       {
         id: 5,
-        word: 'that'
+        word: 'the'
       },
       {
         id: 6,
-        word: 'you'
+        word: 'crowd'
       },
       {
         id: 7,
-        word: 'dream'
+        word: 'buy'
       },
       {
         id: 8,
-        word: 'of'
+        word: 'me (2nd)'
       },
       {
         id: 9,
-        word: 'once'
+        word: 'thumb'
       },
       {
         id: 10,
-        word: 'in'
+        word: 'peanuts'
       },
       {
         id: 11,
-        word: 'a'
+        word: 'and'
       },
       {
         id: 12,
-        word: 'lullaby'
+        word: 'cracker jacks'
       },
     ]
   },
   {
     id: 1,
-    title: 'I Wanna Dance with Thumb-body',
-    intro: 'Oh, I wanna dance with thumb-body, I wanna feel the heat with...',
+    title: 'Christmas is Coming',
+    intro: 'Christmas is coming, the geese...',
     words: [
-            {
+      {
         id: 1,
-        word: 'thumb-body (1)'
+        word: 'are'
       },
       {
         id: 2,
-        word: 'yeah'
+        word: 'getting'
       },
       {
         id: 3,
-        word: 'I'
+        word: 'fat'
       },
       {
         id: 4,
-        word: 'wanna'
+        word: 'please'
       },
       {
         id: 5,
-        word: 'dance'
+        word: 'put'
       },
       {
         id: 6,
-        word: 'with (1)'
+        word: 'a'
       },
       {
         id: 7,
-        word: 'thumb-body (2)'
+        word: 'penny'
       },
       {
         id: 8,
-        word: 'with (2)'
+        word: 'in'
       },
       {
         id: 9,
-        word: 'thumb-body (3)'
+        word: 'the'
       },
       {
         id: 10,
-        word: 'who'
+        word: 'old'
       },
       {
         id: 11,
-        word: 'loves'
+        word: "man's"
       },
       {
         id: 12,
-        word: 'me'
-      }
+        word: 'hat'
+      },
     ]
-  }
+  },
+  // {
+  //   id: 2,
+  //   title: 'Thumbwhere Over the Rainbow',
+  //   intro: 'Thumbwhere over the rainbow, way up...',
+  //   words: [
+  //     {
+  //       id: 1,
+  //       word: 'high'
+  //     },
+  //     {
+  //       id: 2,
+  //       word: 'and'
+  //     },
+  //     {
+  //       id: 3,
+  //       word: 'the'
+  //     },
+  //     {
+  //       id: 4,
+  //       word: 'dreams'
+  //     },
+  //     {
+  //       id: 5,
+  //       word: 'that'
+  //     },
+  //     {
+  //       id: 6,
+  //       word: 'you'
+  //     },
+  //     {
+  //       id: 7,
+  //       word: 'dream'
+  //     },
+  //     {
+  //       id: 8,
+  //       word: 'of'
+  //     },
+  //     {
+  //       id: 9,
+  //       word: 'once'
+  //     },
+  //     {
+  //       id: 10,
+  //       word: 'in'
+  //     },
+  //     {
+  //       id: 11,
+  //       word: 'a'
+  //     },
+  //     {
+  //       id: 12,
+  //       word: 'lullaby'
+  //     },
+  //   ]
+  // },
+  // {
+  //   id: 3,
+  //   title: 'I Wanna Dance with Thumb-body',
+  //   intro: 'Oh, I wanna dance with thumb-body, I wanna feel the heat with...',
+  //   words: [
+  //           {
+  //       id: 1,
+  //       word: 'thumb-body (1)'
+  //     },
+  //     {
+  //       id: 2,
+  //       word: 'yeah'
+  //     },
+  //     {
+  //       id: 3,
+  //       word: 'I'
+  //     },
+  //     {
+  //       id: 4,
+  //       word: 'wanna'
+  //     },
+  //     {
+  //       id: 5,
+  //       word: 'dance'
+  //     },
+  //     {
+  //       id: 6,
+  //       word: 'with (1)'
+  //     },
+  //     {
+  //       id: 7,
+  //       word: 'thumb-body (2)'
+  //     },
+  //     {
+  //       id: 8,
+  //       word: 'with (2)'
+  //     },
+  //     {
+  //       id: 9,
+  //       word: 'thumb-body (3)'
+  //     },
+  //     {
+  //       id: 10,
+  //       word: 'who'
+  //     },
+  //     {
+  //       id: 11,
+  //       word: 'loves'
+  //     },
+  //     {
+  //       id: 12,
+  //       word: 'me'
+  //     }
+  //   ]
+  // }
 ]
 
 export default singingData;
