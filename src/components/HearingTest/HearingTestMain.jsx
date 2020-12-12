21ee70d0b083ddfc71b5fb32dfd84df6e7ff4040
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-11 21:14:00 -0700

    Add sound files to static folder.  Audio clip plays when user clicks on HearingTest images.

--- src/components/HearingTest/HearingTestMain.jsx
+++ src/components/HearingTest/HearingTestMain.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { hearingData } from '../../data/hearingData';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 const Wrapper = styled.div`
   display: flex;
@@ -8,9 +10,49 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const Image = styled.div`
+  width: 200px;
+  cursor: pointer;
+  .hearing-pic {
+    width: 100%;
+  }
+`;
+
+const Images = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+  margin: 20px 0;
+`;
+
 function HearingTestMain({ setDone }) {
-  const [question, setQuestion] = useState(0);
+  const data = useStaticQuery(graphql`
+    query HearingPics {
+      allFile(filter: {relativeDirectory: {eq: "sounds" }}) {
+        edges {
+          node {
+            name
+            childImageSharp {
+              fluid(maxWidth: 300) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  const images = data.allFile.edges;
+  // console.log(images[0].node.name);
+  const getImage = (imageName) => {
+    const result = images.find(image => image.node.name === imageName);
+    return result;
+  }
   
+  const [question, setQuestion] = useState(0);
+  const [imageOne, setImageOne] = useState(null);
+  const [imageTwo, setImageTwo] = useState(null);
   const handleNextClick = () => {
     if (question < 5) {
       setQuestion(prev => prev + 1);
@@ -20,22 +62,47 @@ function HearingTestMain({ setDone }) {
   }
 
   const handlePlayOne = () => {
-    let audio = new Audio(`./${hearingData[question].soundOne}`);
+    let audio = new Audio(`./${hearingData[question].imageOne}.mp3`);
     audio.currentTime = 0;
     audio.play();
   }
 
   const handlePlayTwo = () => {
-    let audio = new Audio(`./${hearingData[question].soundTwo}`);
+    let audio = new Audio(`./${hearingData[question].imageTwo}.mp3`);
     audio.currentTime = 0;
     audio.play();
   }
 
+  useEffect(() => {
+    const first = getImage(hearingData[question].imageOne);
+    const second = getImage(hearingData[question].imageTwo);
+    console.log(first);
+    setImageOne(first);
+    setImageTwo(second);
+    
+  }, [question])
+
+  useEffect(() => {
+    // console.log(imageOne);
+  }, [])
+
   return (
     <Wrapper>
       <h2>{hearingData[question].question}</h2>
-      <button onClick={handlePlayOne}>play one</button>
-      <button onClick={handlePlayTwo}>play two</button>
+      <Images>
+        <Image onClick={handlePlayOne}>
+          {
+            imageOne &&
+            <Img className="hearing-pic" fluid={imageOne.node.childImageSharp.fluid} />
+          }
+        </Image>
+        <Image onClick={handlePlayTwo}>
+          {
+            imageTwo &&
+            <Img className="hearing-pic" fluid={imageTwo.node.childImageSharp.fluid} />
+          }
+        </Image>
+      </Images>
       <button onClick={handleNextClick}>Next</button>
     </Wrapper>
   )
--- src/data/hearingData.js
+++ src/data/hearingData.js
@@ -7,43 +7,31 @@ export const hearingData = [
     question: 'What sounds most like a banana peel?',
     imageOne: 'guitar',
     imageTwo: 'ufo',
-    soundOne: '484344__inspectorj__bike-bell-ding-single-01-01.wav',
-    soundTwo: '81173__mkoenig__horn-honk.wav'
   },
   {
     question: 'Which sound best represents indignation?',
     imageOne: 'flute',
     imageTwo: 'choir',
-    soundOne: '484344__inspectorj__bike-bell-ding-single-01-01.wav',
-    soundTwo: '81173__mkoenig__horn-honk.wav'
   },
   {
     question: 'Think of your favorite bath towel. Now pick a sound.',
     imageOne: 'cow',
     imageTwo: 'radio',
-    soundOne: '484344__inspectorj__bike-bell-ding-single-01-01.wav',
-    soundTwo: '81173__mkoenig__horn-honk.wav'
   },
   {
     question: 'What sounds most like a cloud?',
     imageOne: 'jingle',
     imageTwo: 'crash',
-    soundOne: '484344__inspectorj__bike-bell-ding-single-01-01.wav',
-    soundTwo: '81173__mkoenig__horn-honk.wav'
   },
   {
     question: 'If you were stranded in the desert, what would you most like to hear?',
     imageOne: 'bongo',
     imageTwo: 'trumpet',
-    soundOne: '484344__inspectorj__bike-bell-ding-single-01-01.wav',
-    soundTwo: '81173__mkoenig__horn-honk.wav'
   },
   {
     question: 'If you were required to permanently change your voice, which would you rather your new voice sound like?',
     imageOne: 'chimes',
     imageTwo: 'water',
-    soundOne: '484344__inspectorj__bike-bell-ding-single-01-01.wav',
-    soundTwo: '81173__mkoenig__horn-honk.wav'
   }, 
 ]
 
