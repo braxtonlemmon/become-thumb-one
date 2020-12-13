4743a113e50f67b94d4537cc12817b7348b33d9e
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-13 15:26:48 -0700

    Add content and styling to About and Contact pages.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import Layout from '../components/layout/layout';
 import SEO from '../components/layout/seo';
+import { PageTitle } from '../components/shared/Headings';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  margin: 0 auto;
+  p {
+    text-align: center;
+    color: ${props => props.theme.colors.rawr};
+    margin-bottom: 20px;
+    font-size: ${props => props.theme.fontSizes.two};
+    line-height: ${props => props.theme.fontSizes.three};
+  }
+`;
 
 function AboutPage() {
   return (
     <Layout>
       <SEO title="About" />
-      <p>about</p>
+      <Wrapper>
+        <PageTitle>About</PageTitle>
+        <p>Try to find another place on the internet where you can become thumb one. Try it.</p>
+        <p>This is exactly why you are here. You are very welcome.</p>
+        <p>Good luck!</p>
+      </Wrapper>
     </Layout>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import Layout from '../components/layout/layout';
 import SEO from '../components/layout/seo';
+import { PageTitle } from '../components/shared/Headings';
+import styled from 'styled-components';
+import { HiOutlineMail } from 'react-icons/hi';
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  margin: 0 auto;
+  p {
+    text-align: center;
+    color: ${props => props.theme.colors.rawr};
+    margin-bottom: 20px;
+    font-size: ${props => props.theme.fontSizes.two};
+    line-height: ${props => props.theme.fontSizes.three};
+  }
+
+  #mail-icon > * {
+    fill: ${props => props.theme.colors.tada};
+  }
+`;
+
+const Mail = styled(HiOutlineMail)`
+  color: ${props => props.theme.colors.rawr};
+  cursor: pointer;
+  height: ${props => props.theme.fontSizes.seven};
+  width: ${props => props.theme.fontSizes.seven};
+  margin-bottom: 20px;
+`;
 
 function ContactPage() {
   return (
     <Layout>
       <SEO title="Contact" />
-      <p>contact</p>
+      <Wrapper>
+        <PageTitle>Contact</PageTitle>
+        <p>Are you confused? Weirded out? Tell me about it.</p>
+        <p>Send me an email:</p>
+        <a href="mailto:braxtonlemmon@gmail.com" target="_blank" rel="noopener noreferrer">
+          <Mail id="mail-icon"></Mail>
+        </a>
+        <p>And I will respond thumbtime.</p>
+      </Wrapper>
     </Layout>
   )
 }
