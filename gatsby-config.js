79951655fc5e54c2c1de49472f46f12800ccd60a
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-13 16:56:05 -0700

    Update gatsby-config in preparation for test deploy.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     author: `@braxtonlemmon`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,18 +12,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Become Thumb One`,
+        short_name: `Become Thumb One`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/cow.png`, // This path is relative to the root of the site.
+        icon: `src/images/thumb.png`, // This path is relative to the root of the site.
       },
     },
     {
@@ -35,7 +32,11 @@ module.exports = {
         ],
         display: 'swap'
       }
-    }
+    },
+    'gatsby-plugin-styled-components',
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
