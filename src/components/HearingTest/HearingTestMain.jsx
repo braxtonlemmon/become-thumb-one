547ae5798d0b595a83f4e0d91a17c7a89969f904
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-13 07:34:01 -0700

    Display next button and thumbs up hand when user chooses a sound in HearingTestMain.

--- src/components/HearingTest/HearingTestMain.jsx
+++ src/components/HearingTest/HearingTestMain.jsx
@@ -1,34 +1,82 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import { hearingData } from '../../data/hearingData';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import Button from '../shared/Button';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: relative;
+  h3 {
+    text-align: center;
+    margin: 0 20px;
+    font-size: ${props => props.theme.fontSizes.two};
+    color: ${props => props.theme.colors.rawr};
+  }
+`;
+
+const Images = styled.div`
+  /* width: 100%; */
+  /* height: 400px; */
+  padding: 0 40px;
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  flex-direction: column;
+  gap: 30px;
+  margin: 15px 0;
+  position: relative;
+  /* border: 1px solid red; */
 `;
 
 const Image = styled.div`
-  width: 200px;
+  width: 120px;
   cursor: pointer;
+  padding: 10px;
+  /* border: 1px solid black; */
   .hearing-pic {
     width: 100%;
   }
 `;
 
-const Images = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 50px;
-  margin: 20px 0;
+const HandBox = styled.div`
+  width: 40px;
+  position: absolute;
+  top: 20px;
+  left: 0;
+  opacity: ${props => (props.selected === 'one' || props.selected ==='two') ? '1' : '0'};
+  /* transition: opacity 300ms ease; */
+  ${props => props.selected === 'one' ? 
+    css`
+      top: 20%;
+    `:
+    css`
+      top: 80%;
+    `
+  }
+  z-index: 400;
+`;
+
+const Next = styled(Button)`
+  position: absolute; 
+  right: 30px;
+  top: 200px;
+  display: ${props => props.selected ? 'block' : 'none'};
 `;
 
 function HearingTestMain({ setDone }) {
   const data = useStaticQuery(graphql`
     query HearingPics {
+      file(name: {eq: "thumb_one"}) {
+        childImageSharp {
+          fluid(maxWidth: 200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
       allFile(filter: {relativeDirectory: {eq: "sounds" }}) {
         edges {
           node {
@@ -44,7 +92,7 @@ function HearingTestMain({ setDone }) {
     }
   `);
   const images = data.allFile.edges;
-  // console.log(images[0].node.name);
+  const hand = data.file;
   const getImage = (imageName) => {
     const result = images.find(image => image.node.name === imageName);
     return result;
@@ -53,7 +101,10 @@ function HearingTestMain({ setDone }) {
   const [question, setQuestion] = useState(0);
   const [imageOne, setImageOne] = useState(null);
   const [imageTwo, setImageTwo] = useState(null);
+  const [selected, setSelected] = useState('');
+
   const handleNextClick = () => {
+    setSelected('');
     if (question < 5) {
       setQuestion(prev => prev + 1);
     } else {
@@ -61,13 +112,15 @@ function HearingTestMain({ setDone }) {
     }
   }
 
-  const handlePlayOne = () => {
+  const handlePlayOne = (e) => {
+    setSelected('one');
     let audio = new Audio(`./${hearingData[question].imageOne}.mp3`);
     audio.currentTime = 0;
     audio.play();
   }
 
-  const handlePlayTwo = () => {
+  const handlePlayTwo = (e) => {
+    setSelected('two');
     let audio = new Audio(`./${hearingData[question].imageTwo}.mp3`);
     audio.currentTime = 0;
     audio.play();
@@ -82,28 +135,27 @@ function HearingTestMain({ setDone }) {
     
   }, [question])
 
-  useEffect(() => {
-    // console.log(imageOne);
-  }, [])
-
   return (
     <Wrapper>
-      <h2>{hearingData[question].question}</h2>
+      <h3>{hearingData[question].question}</h3>
       <Images>
-        <Image onClick={handlePlayOne}>
+        <Image onClick={(e) => handlePlayOne(e)} selected={selected === 'one'}>
           {
             imageOne &&
             <Img className="hearing-pic" fluid={imageOne.node.childImageSharp.fluid} />
           }
         </Image>
-        <Image onClick={handlePlayTwo}>
+        <HandBox selected={selected}>
+          <Img fluid={hand.childImageSharp.fluid} />
+        </HandBox>
+        <Image onClick={(e) => handlePlayTwo(e)} selected={selected === 'two'}>
           {
             imageTwo &&
             <Img className="hearing-pic" fluid={imageTwo.node.childImageSharp.fluid} />
           }
         </Image>
       </Images>
-      <button onClick={handleNextClick}>Next</button>
+      <Next onClick={handleNextClick} selected={selected}>Next</Next>
     </Wrapper>
   )
 }
