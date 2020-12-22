a42935348bf12cf0734c6d82e4f61b5d573c59ba
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-22 10:46:23 -0700

    Implement ThumbModal with click a dot page.

--- src/components/ClickADot/ClickADotModal.jsx
+++ src/components/ClickADot/ClickADotModal.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from '../shared/Button';
-import { ModalWrapper, ModalBox } from '../shared/Modal';
-
-const Wrapper = styled(ModalWrapper)``;
+import { ModalBox } from '../shared/Modal';
 
 const IntroBox = styled(ModalBox)``;
 
@@ -13,13 +11,11 @@ function ClickADotModal({ setIntroOpen }) {
   }
   
   return (
-    <Wrapper>
-      <IntroBox>
-        <p>Time to test your thumb endurance.</p>
-        <p>Using a mouse? Yes, you still need to click with your thumb.</p>
-        <Button onClick={handleCloseClick}>Ready to click...</Button>
-      </IntroBox>
-    </Wrapper>
+    <IntroBox>
+      <p>Time to test your thumb endurance.</p>
+      <p>Using a mouse? Yes, you still need to click with your thumb.</p>
+      <Button onClick={handleCloseClick}>Ready to click...</Button>
+    </IntroBox>
   )
 }
 
--- src/pages/click-a-dot.js
+++ src/pages/click-a-dot.js
@@ -4,16 +4,26 @@ import SEO from '../components/layout/seo';
 import ClickADotModal from '../components/ClickADot/ClickADotModal';
 import ClickADotMain from '../components/ClickADot/ClickADotMain';
 import ClickADotDone from '../components/ClickADot/ClickADotDone';
+import ThumbModal from '../components/ThumbModal';
+
 function ClickAButtonPage() {
   const [isIntroOpen, setIntroOpen] = useState(true);
   const [isDone, setDone] = useState(false);
+  const handleIntroClose = () => {
+    setIntroOpen(false);
+  }
   
   return (
     <Layout>
       <SEO title="Thumb Singing" />
       {
         isIntroOpen &&
-        <ClickADotModal setIntroOpen={setIntroOpen} />
+        <ThumbModal
+          isOpen={isIntroOpen}
+          handleClose={handleIntroClose}
+        >
+          <ClickADotModal setIntroOpen={setIntroOpen} />
+        </ThumbModal>
       }
       <ClickADotMain 
         setDone={setDone}
