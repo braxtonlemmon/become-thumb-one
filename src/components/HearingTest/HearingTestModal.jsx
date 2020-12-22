046ebb5fcd50cdf948786e49434683823e709d00
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-22 10:27:48 -0700

    Implement ThumbModal with HearingTest

--- src/components/HearingTest/HearingTestModal.jsx
+++ src/components/HearingTest/HearingTestModal.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ModalWrapper, ModalBox } from '../shared/Modal';
+import { ModalBox } from '../shared/Modal';
 import Button from '../shared/Button';
-
-const Wrapper = styled(ModalWrapper)``;
 
 const IntroBox = styled(ModalBox)``;
 
@@ -13,14 +11,12 @@ function HearingTestModal({ setIntroOpen }) {
   }
   
   return (
-    <Wrapper>
-      <IntroBox>
-        <p>This is a simple hearing test.</p>
-        <p>You will be asked a question and can respond with one of two sound choices.</p>
-        <p>To listen and choose a sound, just click on its associated picture.</p>
-        <Button onClick={handleCloseClick}>Begin</Button>
-      </IntroBox>
-    </Wrapper>
+    <IntroBox>
+      <p>This is a simple hearing test.</p>
+      <p>You will be asked a question and can respond with one of two sound choices.</p>
+      <p>To listen and choose a sound, just click on its associated picture.</p>
+      <Button onClick={handleCloseClick}>Begin</Button>
+    </IntroBox>
   )
 }
 
--- src/pages/hearing-test.js
+++ src/pages/hearing-test.js
@@ -4,17 +4,27 @@ import SEO from '../components/layout/seo';
 import HearingTestModal from '../components/HearingTest/HearingTestModal';
 import HearingTestMain from '../components/HearingTest/HearingTestMain';
 import HearingTestOutro from '../components/HearingTest/HearingTestOutro';
+import ThumbModal from '../components/ThumbModal';
 
 function HearingTestPage() {
   const [introOpen, setIntroOpen] = useState(true);
   const [isDone, setDone] = useState(false);
 
+  const handleIntroClose = () => {
+    setIntroOpen(false);
+  }
+
   return (
     <Layout>
       <SEO title="Hearing test" />
       {
-        introOpen &&
-        <HearingTestModal setIntroOpen={setIntroOpen} />
+        introOpen &&  
+        <ThumbModal
+          isOpen={introOpen}
+          handleClose={handleIntroClose}
+        >
+          <HearingTestModal setIntroOpen={setIntroOpen} />
+        </ThumbModal>
       }
       {
         !isDone && 
