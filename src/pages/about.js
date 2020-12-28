9998fde30e6f67908696d4c70cbfbef6e1b00a38
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-28 10:16:24 -0700

    Add Github icon to about page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout/layout';
 import SEO from '../components/layout/seo';
 import { PageTitle } from '../components/shared/Headings';
 import styled from 'styled-components';
+import { FaGithub } from 'react-icons/fa';
 
 const Wrapper = styled.section`
   display: flex;
@@ -17,6 +18,18 @@ const Wrapper = styled.section`
     margin-bottom: 20px;
     font-size: ${props => props.theme.fontSizes.two};
     line-height: ${props => props.theme.fontSizes.three};
+  }
+`;
+
+const GitHubIcon = styled(FaGithub)`
+  color: ${props => props.theme.colors.hey};
+  cursor: pointer;
+  height: 48px;
+  width: 48px;
+  transform: rotate(0);
+  transition: transform 300ms ease;
+  &:hover {
+    transform: rotate(7deg);
   }
 `;
 
@@ -38,6 +51,9 @@ function AboutPage() {
         <p>Try to find another place on the internet where you can become thumb one. Try it.</p>
         <p>This is exactly why you are here. You are very welcome.</p>
         <p>Good luck!</p>
+        <a href="https://github.com/braxtonlemmon/become-thumb-one" target="_blank" rel="noopener noreferrer">
+          <GitHubIcon />
+        </a>
       </Wrapper>
     </Layout>
   )
