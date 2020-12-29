e64632dbcefa7c1ea54f8083c1713b7ae083cf51
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-28 18:05:48 -0700

    Add scrambled mode to required reading modal.

--- src/components/RequiredReading/ReadingModal.jsx
+++ src/components/RequiredReading/ReadingModal.jsx
@@ -32,7 +32,7 @@ const ToggleButtons = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: 8px;
+  gap: 2px;
   margin-bottom: 15px;
   @media (min-width: 450px) {
     flex-direction: row;
@@ -42,9 +42,10 @@ const ToggleButtons = styled.div`
 const ModeButton = styled.button`
   font-family: ${props => props.theme.fonts.galindo};
   cursor: pointer;
-  background: ${props => props.vowels ? props.theme.colors.hey : 'none'};
-  border: ${props => props.vowels ? `3px solid ${props.theme.colors.rawr}` : `2px solid ${props.theme.colors.hey}`};
-  color: ${props => props.vowels ? props.theme.colors.rawr : props.theme.colors.hey};
+  background: ${props => props.chosen ? props.theme.colors.hey : 'none'};
+  border: ${props => props.chosen ? `3px solid ${props.theme.colors.rawr}` : `2px solid ${props.theme.colors.hey}`};
+  color: ${props => props.chosen ? props.theme.colors.rawr : props.theme.colors.hey};
+  border-radius: 5px;
 `;
 
 const Toggle = styled.div`
@@ -87,10 +88,12 @@ const DoneButton = styled(Button)``;
 
 function ReadingModal({ setModalOpen, book, setBookRead, modalOpen }) {
   const [vowels, setVowels] = useState(true);
+  const [consonants, setConsonants] = useState(false);
+  const [scrambled, setScrambled] = useState(false);
   const [doneX, setDoneX] = useState(25);
   const [doneY, setDoneY] = useState(60);
   const [doneCount, setDoneCount] = useState(0);
-
+  
   const doneMessages = [
     "Done.",
     "I'm done.",
@@ -98,23 +101,53 @@ function ReadingModal({ setModalOpen, book, setBookRead, modalOpen }) {
     "Seriously!",
     "Ugh."
   ]
-
+  
   const toVowels = (text) => {
     return text.replace(/[bcdfghjklmnpqrstvwxyz]/ig, "");
   }
-
+  
   const toConsonants = (text) => {
     return text.replace(/[aeiou]/ig, "");
   }
-
+  
+  const scrambleWord = (theWord) => {
+    const isAlpha = (ch) => { return /^[A-Z]$/i.test(ch) }
+    let word, lastChar;
+    if (isAlpha(theWord[theWord.length -1])) {
+      word = theWord;
+      lastChar = ''
+    } else {
+      word = theWord.slice(0, theWord.length - 1);
+      lastChar = theWord[theWord.length -1];
+    }
+    let middle = word.slice(1, word.length - 1).split('');
+    console.log(middle);
+    for (let i = middle.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [middle[i], middle[j]] = [middle[j], middle[i]];
+    }
+    return `${word[0]}${middle.join('')}${word[word.length - 1]}${lastChar}`;
+  }
+  
+  const toScrambled = (text) => {
+    const words = text.split(' ');
+    const scrambledWords = words.map(word => {
+      if (word.length > 2) {
+        return scrambleWord(word);
+      }
+      return word;
+    })
+    return scrambledWords.join(' ');
+  }
+  
   const randomPercent = () => {
     return Math.floor(Math.random() * 55);
   }
-
+  
   const toggle = () => {
     setVowels(prev => !prev);
   }
-
+  
   const handleDoneClick = () => {
     setDoneX(randomPercent());
     setDoneY(randomPercent());
@@ -125,22 +158,29 @@ function ReadingModal({ setModalOpen, book, setBookRead, modalOpen }) {
       setModalOpen(false);
     }
   }
-
+  
   const handleCloseClick = () => {
     setModalOpen(false);
   }
-
+  
   const onlyVowels = toVowels(book.text);
   const onlyConsonants = toConsonants(book.text);
-  
+  const onlyScrambled = toScrambled(book.text);
+  const [content, setContent] = useState(onlyVowels);
+
   return (
     <TextBox>
       <h2>{book.title}</h2>
       <Close onClick={handleCloseClick}>x</Close>
       <ToggleButtons>
         <ModeButton 
-          onClick={() => setVowels(true)}
-          vowels={vowels}
+          onClick={() => {
+            setVowels(true);
+            setConsonants(false);
+            setScrambled(false);
+            setContent(onlyVowels)
+          }}
+          chosen={vowels}
           >Vowel</ModeButton>
         {/* <Toggle 
           vowels={vowels} 
@@ -149,14 +189,28 @@ function ReadingModal({ setModalOpen, book, setBookRead, modalOpen }) {
           <button className="toggle-dot"></button>
         </Toggle> */}
         <ModeButton 
-          onClick={() => setVowels(false)}
-          vowels={!vowels}
+          onClick={() => {
+            setVowels(false)
+            setConsonants(true);
+            setScrambled(false);
+            setContent(onlyConsonants);
+          }}
+          chosen={consonants}
         >
           Consonant
         </ModeButton>
+        <ModeButton
+          onClick={() => {
+            setVowels(false);
+            setConsonants(false);
+            setScrambled(true);
+            setContent(onlyScrambled);
+          }}
+          chosen={scrambled}
+        >Scrambled</ModeButton>
       </ToggleButtons>
       <Text>
-        {vowels ? onlyVowels : onlyConsonants}
+        {content}
       </Text>
       <Bottom>
         <DoneButton onClick={handleDoneClick} x={doneX} y={doneY}>{doneMessages[doneCount]}</DoneButton>
